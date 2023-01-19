4259e2f8864d92b832874f92931ef4baa7fa890f
Himanshu Gupta <himanshugupta558@gmail.com>
2023-01-19 17:23:39 +0530

    Added error-codes in controller

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,4 +1,5 @@
 const UserService = require('../services/user-service');
+const { SuccessCodes } = require('../utils/error-codes');
 
 const userService = new UserService();
 
@@ -8,7 +9,7 @@ const create = async (req, res) => {
             email: req.body.email,
             password: req.body.password,
         });
-        return res.status(201).json({
+        return res.status(SuccessCodes.CREATED).json({
             message: 'User created successfully',
             data: response,
             success: true,
@@ -28,7 +29,7 @@ const create = async (req, res) => {
 const signIn = async (req, res) => {
     try {
         const response = await userService.signIn(req.body.email, req.body.password);
-        return res.status(200).json({
+        return res.status(SuccessCodes.OK).json({
             message: 'Sign in successfully',
             data: response,
             success: true,
@@ -50,7 +51,7 @@ const isAuthenticated = async (req, res) => {
     try {
         const token = req.headers['x-access-token'];
         const response = await userService.isAuthenticated(token);
-        return res.status(200).json({
+        return res.status(SuccessCodes.OK).json({
             success: true,
             err: {},
             data: response,
